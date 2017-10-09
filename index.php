baeea3892c90493aecd77fea9954c902a8c21ffd
Matt David <mcdav3636@gmail.com>
2017-10-09 16:20:11 -0600

    added unordered lists

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 	</head>
 	<body>
 		<h1>About me</h1>
-		<p>My name is Matt</p>
+		<p>My name is Matt. I like to make things as much fun as possible</p>
+		<h2>My strengths</h2>
+		<ul>
+			<li>heating up frozen food</li>
+			<li>shredding newspaper</li>
+			<li>communicating with large predatory cats</li>
+		</ul>
+				<h2>My Weaknesses</h2>
+			<p>
+				<ul>
+					<li>cleaning the kitchen</li>
+					<li>money management</li>
+					<li>making directories</li>
+				</ul>
+
 	</body>
 </html>
