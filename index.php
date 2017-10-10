06b0d2164806ec5cc89eb0425741b3bab09d625d
Matt David <mcdav3636@gmail.com>
2017-10-09 22:19:10 -0600

    changed image name and alt description

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title> About Me</title>
-		<link rel="stylesheet" href="./styles/styles.css">
+		<link rel="stylesheet" href="./styles/styles.css"/>
 	</head>
 	<body>
 
-		<img src="image/me_at_my_best.jpg" alt="goofy picture of me" height="800" width="800">
+		<img src="image/goofy-me.jpg" alt="goofy picture of me" height="800" width="800"/>
 		<h1>About me</h1>
 		<p>My name is Matt. I like to make things as much fun as possible. When things start to get bland I like to make
 		it exciting</p>
+
 		<div>
 			<h2>My strengths</h2>
+
 		<ul class="center-list">
 			<li>heating up frozen food</li>
 			<li>starting fires</li>
 			<li>communicating with large predatory cats</li>
 		</ul>
+
 				<h2>My Weaknesses</h2>
-			<p>
+
 				<ul class="center-list">
 					<li>cleaning the kitchen</li>
 					<li>money management</li>
